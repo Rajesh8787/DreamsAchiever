9ae89a692326b11a20b93e06b6205417eb495a17
unknown <Beast@DESKTOP-FMNP9KP>
2024-07-26 16:30:26 +0530

    Coding Part starting

---
+++ src/main/java/org/TwoPointers/TwoSumArraysUsingBruteForceApproach.java
@@ -0,0 +1,34 @@
+package main.java.org.TwoPointers;
+
+/*Problem Statement: Given an array, find the indexes of two elements whose sum is equal to the given sum. If there exist multiple solutions, print the minimum indices.
+
+        Here we are assuming the array is sorted.
+
+        Examples:
+
+        Example 1:
+
+        Input: arr[]={1,3,4,5,7,10,11,19,20}, sum=7
+        Output: {1,2}
+
+        Example 2:
+        Input: arr[]={2,9,13,21,54}, sum=63
+        Output: {1,4}
+        */
+
+
+//First .Using Brute force approach
+
+public void TwoSum(int arr[]){
+
+        };
+
+
+public class TwoSumArraysUsingBruteForceApproach {
+    public static void main(String[] args){
+        int arr[]={1,3,4,5,7,10,11,19,20};
+        TwoSumArraysUsingBruteForceApproach obj = new TwoSumArraysUsingBruteForceApproach();
+        obj.
+
+    }
+}
